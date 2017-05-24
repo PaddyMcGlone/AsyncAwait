0acc067e73db972e3189834dbd145b7356e82077
paddymcglone <paddymcglone@gmail.com>
2017-05-24 19:28:06 +0100

    Adding a simple MVC example, mocked the web service delayed calls

---
+++ AsyncAndAwaitMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using AsyncAndAwaitMVCProject.Models;
+
+namespace AsyncAndAwaitMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var model = new HomePageModel();
+            model.Message.Add($"Starting Action now: {DateTime.Now}");
+            await GetAsyncHeadLines();
+            await GetAsyncWeather();
+            model.Message.Add($"Completing Action now: {DateTime.Now}");
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        #region Helper Methods
+
+        private Task GetAsyncWeather()
+        {
+           return Task.Delay(3000);
+        }
+
+        private Task GetAsyncHeadLines()
+        {
+            return Task.Delay(5000);
+        }
+        #endregion
+    }
+}
---
+++ AsyncAndAwaitMVCProject/Models/HomePageModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AsyncAndAwaitMVCProject.Models
+{
+    public class HomePageModel
+    {
+        public HomePageModel()
+        {
+            Message = new List<string>();    
+        }
+
+        public List<string> Message { get; set; }
+    }
+}
