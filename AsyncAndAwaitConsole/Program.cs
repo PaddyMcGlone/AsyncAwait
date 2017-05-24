b472797279da9960ab0a6149c6921d6e75865b5f
paddymcglone <paddymcglone@gmail.com>
2017-05-24 18:59:23 +0100

    Adding simple console app

---
+++ AsyncAndAwaitConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwaitConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            var task = SlowOperationAsync();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine($"Slow operation result {task.Result}");
+            Console.WriteLine($"Main completed on thread: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        /// <summary>
+        /// An example Async method, of type Task, as Async always return
+        /// a type of Task
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<int> SlowOperationAsync()
+        {
+            Console.WriteLine($"Slow operation started on: {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(3000);
+            Console.WriteLine($"Slow operation completed on: {Thread.CurrentThread.ManagedThreadId}");
+            return 42;
+        }
+    }
+}
